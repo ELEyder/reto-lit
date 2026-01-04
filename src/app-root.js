5d9aab59d1d83a6c18bc16d0a4547a51e5f6079a
ELEyder <74992528+ELEyder@users.noreply.github.com>
2026-01-04 11:38:51 -0500

    Add top and default countries

--- src/app-root.js
+++ src/app-root.js
@@ -1,11 +1,13 @@
 import { LitElement, html } from "lit";
 import "./features/header/my-header";
+import "./features/top/my-top";
 
 export class AppRoot extends LitElement {
 
   render() {
     return html`
       <main>
+        <my-top></my-top>
         <my-header></my-header>
       </main>
     `;
