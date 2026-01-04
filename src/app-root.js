b3752f64b05cd951ddced85ef47ac0a1abd915b2
ELEyder <74992528+ELEyder@users.noreply.github.com>
2026-01-04 10:50:20 -0500

    Add logo and navigation links

--- src/app-root.js
+++ src/app-root.js
@@ -1,62 +1,16 @@
-import { LitElement, css, html } from "lit";
+import { LitElement, html } from "lit";
+import "./features/header/my-header";
 
 export class AppRoot extends LitElement {
-  static get properties() {
-    return {
-      docsHint: { type: String },
-
-      count: { type: Number },
-    };
-  }
-
-  constructor() {
-    super();
-    this.docsHint = "Click on the Vite and Lit logos to learn more";
-    this.count = 0;
-  }
 
   render() {
     return html`
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} class="button">
-          count is ${this.count}
-        </button>
-        <button @click=${this._handleClick} class="confirm">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
+      <main>
+        <my-header></my-header>
+      </main>
     `;
   }
-
-  _onClick() {
-    this.count++;
-  }
-
-  _handleClick() {
-    this.count--;
-  }
-
-  static get styles() {
-    return css`
-    .button {
-      font-size: 1.2rem;
-      padding: 0.6rem 1.2rem;
-      border-radius: 8px;
-      color: white;
-      background-color: #0070f3;
-    }
-
-    .confirm {
-      font-size: 1.2rem;
-      padding: 0.6rem 1.2rem;
-      border-radius: 8px;
-      color: white;
-      background-color: #ff0000;
-    }
-    `;
-  }
+  
 }
 
 window.customElements.define("app-root", AppRoot);
---
+++ src/features/header/my-header.js
@@ -0,0 +1,93 @@
+import { css, html, LitElement } from "lit";
+import { Star } from "lucide-static";
+import { Home } from "lucide-static";
+
+export class MyHeader extends LitElement {
+  static get properties() {
+    return {};
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <header>
+        <div class="container-header">
+          <a href="/">
+            <img class="logo" src="./logo.svg" alt="Vite logo" />
+          </a>
+          <ul>
+            <li>
+              <a href="/">
+                <span class="icon" .innerHTML=${Home}></span>Inicio</a
+              >
+            </li>
+            <li>
+              <a href="/favorites">
+                <span class="icon" .innerHTML=${Star}></span>Favoritos</a
+              >
+            </li>
+          </ul>
+        </div>
+      </header>
+    `;
+  }
+
+  static get styles() {
+    return css`
+      header {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        background-color: #f0f0f0;
+      }
+
+      ul {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+      }
+
+      li {
+        list-style: none;
+        position: relative;
+        font-weight: 500;
+        border-bottom: 2px solid transparent;
+        transition: all 0.2s ease;
+      }
+
+      a,
+      a:active {
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        padding: 0.5rem 1rem;
+        gap: 0.5rem;
+        color: #333;
+      }
+
+      a:hover {
+        color: blueviolet;
+      }
+
+      li:hover {
+        color: blueviolet;
+        border-bottom: 2px solid currentColor;
+      }
+
+      .container-header {
+        display: flex;
+        width: 100%;
+        align-items: center;
+        gap: 2rem;
+        max-width: 1200px;
+        height: 121px;
+        margin: auto;
+      }
+    `;
+  }
+}
+
+window.customElements.define("my-header", MyHeader);
